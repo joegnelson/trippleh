41a3c51259fcfc9a2c819efb503e3bef1ea1dec4
Nelson, Joe <JNelson-consultant@Scholastic.com>
2017-07-04 13:12:00 -0400

    Initial Commit

---
+++ FirstTabbedApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FirstTabbedApp
+//
+//  Created by Nelson, Joe on 7/4/17.
+//  Copyright © 2017 Nelson, Joe. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

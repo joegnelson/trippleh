296e56bcc491b8431208cd8c192e9a2e6b053974
Hailey Nelson <Hailey@Haileys-Air.home>
2017-09-03 23:24:13 -0400

    setup search view

--- FirstTabbedApp/SearchViewController.swift
+++ FirstTabbedApp/SearchViewController.swift
@@ -10,22 +10,37 @@ import UIKit
 
 class SearchViewController: UITableViewController {
     
+    let catagories:[[String]] = [["All Meals", "Breakfast", "Lunch/Dinner"],
+                                 ["All Desserts", "Ice Cream", "Batters","Doughs"],
+                                 ["Chips", "Dips", "Granola", "Bars and Balls"]]
+    
+    let headers:[String] = ["Meal", "Dessert", "Snacks"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("ICU")
+        
+        self.navigationItem.title = "Search"
     }
- /*
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+ 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return catagories[section].count
+    }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return catagories.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = "It's a cell"
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = catagories[indexPath.section][indexPath.row]
         return cell
     }
-*/    
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return headers[section]
+    }
+ 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
     }
 }

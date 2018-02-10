f8e418e23fe501443200c4d80de24939121af55a
Nelson, Joe <JNelson-consultant@Scholastic.com>
2018-02-10 18:03:50 -0500

    (BUG) Filter screen crashing

--- FirstTabbedApp/SettingsViewController.swift
+++ FirstTabbedApp/SettingsViewController.swift
@@ -11,14 +11,14 @@ import UIKit
 class SettingsViewController: UIViewController {
     //Outlet- talks to view by using outlet
     //Action- view pass on messages about view to ViewController by using action; updates a label
-    /**
+    
     //Switches:
     @IBAction func noSugarSwitch(_ sender: UISwitch) {
         if (sender.isOn == false){
-            noSugarFilter = false
+//            noSugarFilter = false
         }
         else {
-            noSugarFilter = true
+//            noSugarFilter = true
         }
     }
     @IBAction func noGlutenSwitch(_ sender: UISwitch) {
@@ -31,7 +31,7 @@ class SettingsViewController: UIViewController {
     }
     @IBAction func noDairySwitch(_ sender: UISwitch) {
     }
-    */
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

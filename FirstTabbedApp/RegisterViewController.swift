673ae792dbd544c3323b6f323e458e9a8021c09f
Nelson, Joe <JNelson-consultant@Scholastic.com>
2018-09-23 14:24:47 -0400

    Client:Register : added status code checks

--- FirstTabbedApp/RegisterViewController.swift
+++ FirstTabbedApp/RegisterViewController.swift
@@ -43,46 +43,70 @@ class RegisterViewController: UIViewController, UITextFieldDelegate {
         let request = URL(string: "http://ccc-restrictless-login-t1.appspot.com/register?username=\(_user.text!)&pass=\(_pass.text!)&email=\(_email.text!)")!
         RegisterViewController.query(obj: self,url: request,completion: completionX)
     }
-    func completionX(obj:Any,data: Data?, error: Error?)->Void{
-        guard let data = data, error == nil else { return }
-        let responseString = String(data: data, encoding: .utf8)
-        print(responseString ?? "responseString=nil")
-        DispatchQueue.main.async() { () -> Void in
-            if let obj = obj as? RegisterViewController{
-                do {
-                    let decoder = JSONDecoder()
-                    user = try decoder.decode(User.self, from: data)
-                    print("email:\(user?.email ?? "")")
-                    obj._label.text="Registration Success"
-                } catch {
-                    //handle error
-                    print(error)
-                }
-
-            }
+    func completionX(obj:Any,data: Data?, error: Error?,_ statusCode:Int)->Void{
+        if let data = data{
+            let responseString = String(data: data, encoding: .utf8)
+            print(responseString ?? "responseString=nil")
         }
+            DispatchQueue.main.async() { () -> Void in
+                if let obj = obj as? RegisterViewController{
+                    do {
+                        if(statusCode == 200){
+                            let responseString = String(data: data!, encoding: .utf8)
+                            print(responseString ?? "resonseString=nil")
+//                            let jsonData=responseString?.data(using: .utf8)!
+//                            print(jsonData ?? "jsonData=nil")
+
+                            //let responseString = String(data: data!, encoding: .utf8)
+                            let decoder = JSONDecoder()
+                            
+                            user = try decoder.decode(User.self, from: data!)
+                            print("email:\(user?.email ?? "")")
+                            obj._label.text="Registration Success"
+                        }else if(statusCode == 403){
+                            obj._label.text="This UserName or Email is already registered"
+
+                        }else if(statusCode == 403){
+                            obj._label.text="This Email is already registered"
+                        }else{
+                            obj._label.text="Unknow Error"
+
+                        }
+                        
+                    } catch {
+                        //handle error
+                        print(error)
+                    }
+
+                }
+            }
         
     }
 
-    static func query(obj:Any,url: URL, completion: @escaping (_ obj:Any, _ data: Data?, _ error: Error? ) -> Void) {
+    static func query(obj:Any,url: URL, completion: @escaping (_ obj:Any, _ data: Data?, _ error: Error? ,_ httpStatus:Int) -> Void) {
         HomeViewController.getDataFromUrl(url: url) { data, response, error in
             if let error = error {
-                completion(obj, nil, error)
+                completion(obj, nil, error,0)
                 return
             }
             
-            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
-                print("statusCode should be 200, but is \(httpStatus.statusCode)")
-                print(response ?? "response is nil")
-                return
+            if let httpStatus = response as? HTTPURLResponse {
+                print(httpStatus.statusCode)
+                if(httpStatus.statusCode != 200){
+                    print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                    print(response ?? "response is nil")
+                    completion(obj, nil, error,httpStatus.statusCode)
+                    return
+                }
+                if let data = data {
+                    completion(obj, data, nil,httpStatus.statusCode)
+                } else {
+                    completion(obj, nil, nil,httpStatus.statusCode)
+                }
             }
 
             
-            if let data = data {
-                completion(obj, data, nil)
-            } else {
-                completion(obj, nil, nil)
-            }
+            
         }
 
     }

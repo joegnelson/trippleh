a257173ad52d3d2e5a14851de0a1f6fa02a67901
Nelson, Joe <JNelson-consultant@Scholastic.com>
2018-10-03 21:39:55 -0400

    reset password

--- FirstTabbedApp/ResetPassViewController.swift
+++ FirstTabbedApp/ResetPassViewController.swift
@@ -20,6 +20,7 @@ class ResetPassViewController: UIViewController {
     @IBOutlet weak var _pass: UITextField!
     @IBOutlet weak var _label: UILabel!
     
+    var failCount=0
     
     @IBAction func resetButtonAct(_ sender: Any) {
         
@@ -30,11 +31,37 @@ class ResetPassViewController: UIViewController {
         if (digit1.text!.isEmpty || digit2.text!.isEmpty || digit3.text!.isEmpty || digit4.text!.isEmpty) {
             _label.text = "Check email for security code."
         }
-        else {
-            //Segue
-            performSegue(withIdentifier: "resetPasswordComplete", sender: self)
-        }
+//        else {
+//            //Segue
+//            performSegue(withIdentifier: "resetPasswordComplete", sender: self)
+//        }
         
+        
+        
+        //CREATE REQUUEST
+        let request2 = URLRequest(url: URL(string: "\(baseDomain)/resetpassword?email=\(_email.text!)&pass=\(_pass.text!)&code=\(fourDigitCode!)")!)
+        
+        //CREATE TASK
+        let task = URLSession.shared.dataTask(with: request2) { data, response, error in guard (data != nil), error == nil else {
+            print("error = \(String(describing: error))")
+            return
+            }
+            
+            DispatchQueue.main.async {
+                if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                    self.failCount=self.failCount+1;
+                    self._label.text = "Invalid Code OR Email. \(self.failCount)"
+                    return
+                }
+                //Segue
+                self.performSegue(withIdentifier: "resetPasswordComplete", sender: self)
+                self._label.text = ""
+                self.failCount=0
+            }
+        }
+        //EXECUTE TASK
+        task.resume()
+
     }
     
     override func viewDidLoad() {
